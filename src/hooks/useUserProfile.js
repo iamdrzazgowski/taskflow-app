c3052f398b5f4e3ff42d488b45dcc0798643dc9e
Adrian Drzazgowski <iamdrzazgowski@gmail.com>
2025-05-03 08:08:00 +0200

    FIX infinity fetching user data from db

---
+++ src/hooks/useUserProfile.js
@@ -0,0 +1,43 @@
+import React, { useEffect, useState } from 'react';
+import { useNavigate } from 'react-router';
+import { useAuth } from '../contexts/AuthProvider';
+import supabase from '../utils/supabaseClient';
+
+export function useUserProfile() {
+    const [profile, setProfile] = useState(null);
+    const [isLoading, setIsLoading] = useState(true);
+    const { user } = useAuth();
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        const fetchProfile = async () => {
+            if (!user || profile) return;
+
+            setIsLoading(true);
+
+            const { data, error } = await supabase
+                .from('users')
+                .select('*')
+                .eq('id', user.id)
+                .maybeSingle();
+
+            if (error) {
+                console.error('Błąd przy pobieraniu profilu:', error);
+                setIsLoading(false);
+                return;
+            }
+
+            if (!data) {
+                navigate('/login');
+                return;
+            }
+
+            setProfile(data);
+            setIsLoading(false);
+        };
+
+        fetchProfile();
+    }, [user, navigate, profile]);
+
+    return { profile, isLoading };
+}
--- src/pages/AppLayout/AppLayout.jsx
+++ src/pages/AppLayout/AppLayout.jsx
@@ -1,45 +1,12 @@
-import React, { useEffect, useState } from 'react';
-import { Link, Outlet, useNavigate } from 'react-router';
+import React from 'react';
+import { Link, Outlet } from 'react-router';
 import Navbar from '../../components/Navbar/Navbar';
 import supabase from '../../utils/supabaseClient';
-import { useAuth } from '../../contexts/AuthProvider';
 import LoadingScreen from '../LoadingScreen/LoadingScreen';
+import { useUserProfile } from '../../hooks/useUserProfile';
 
 export default function AppLayout() {
-    const [profile, setProfile] = useState(null);
-    const [isLoading, setIsLoading] = useState(true);
-    const { user } = useAuth();
-    const navigate = useNavigate();
-
-    useEffect(() => {
-        const fetchProfile = async () => {
-            if (!user || profile) return;
-
-            setIsLoading(true);
-
-            const { data, error } = await supabase
-                .from('users')
-                .select('*')
-                .eq('id', user.id)
-                .maybeSingle();
-
-            if (error) {
-                console.error('Błąd przy pobieraniu profilu:', error);
-                setIsLoading(false);
-                return;
-            }
-
-            if (!data) {
-                navigate('/login');
-                return;
-            }
-
-            setProfile(data);
-            setIsLoading(false);
-        };
-
-        fetchProfile();
-    }, [user, navigate, profile]);
+    const { profile, isLoading } = useUserProfile();
 
     const handleLogout = async () => {
         const { error } = await supabase.auth.signOut();
